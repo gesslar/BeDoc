6f80e8a61d43c5a04528c35fc05df5a37b69bb81
gesslar <1266935+gesslar@users.noreply.github.com>
2025-01-15 13:46:36 -0500

    temp testing thingie
    A  docs.20250115/.github/workflows/pages-deploy.yml
    A  docs.20250115/.nojekyll
    A  docs.20250115/.vscode/extensions.json
    A  docs.20250115/.vscode/settings.json
    A  docs.20250115/.vscode/tasks.json
    A  docs.20250115/Gemfile
    A  docs.20250115/Gemfile.lock
    A  docs.20250115/LICENSE
    A  docs.20250115/README.md
    A  docs.20250115/_config.yml
    A  docs.20250115/_data/contact.yml
    A  docs.20250115/_data/share.yml
    A  docs.20250115/_plugins/posts-lastmod-hook.rb
    A  docs.20250115/_posts/.placeholder
    A  docs.20250115/_site/404.html
    A  docs.20250115/_site/about/index.html
    A  docs.20250115/_site/app.min.js
    A  docs.20250115/_site/archives/index.html
    A  docs.20250115/_site/assets/css/jekyll-theme-chirpy.css
    A  docs.20250115/_site/assets/css/jekyll-theme-chirpy.css.map
    A  docs.20250115/_site/assets/img/favicons/android-chrome-192x192.png
    A  docs.20250115/_site/assets/img/favicons/android-chrome-512x512.png
    A  docs.20250115/_site/assets/img/favicons/apple-touch-icon.png
    A  docs.20250115/_site/assets/img/favicons/browserconfig.xml
    A  docs.20250115/_site/assets/img/favicons/favicon-16x16.png
    A  docs.20250115/_site/assets/img/favicons/favicon-32x32.png
    A  docs.20250115/_site/assets/img/favicons/favicon.ico
    A  docs.20250115/_site/assets/img/favicons/mstile-150x150.png
    A  docs.20250115/_site/assets/img/favicons/site.webmanifest
    A  docs.20250115/_site/assets/index.html
    A  docs.20250115/_site/assets/js/data/mathjax.js
    A  docs.20250115/_site/assets/js/data/search.json
    A  docs.20250115/_site/assets/js/data/swconf.js
    A  docs.20250115/_site/categories/index.html
    A  docs.20250115/_site/feed.xml
    A  docs.20250115/_site/hooks.html
    A  docs.20250115/_site/index.html
    A  docs.20250115/_site/norobots/index.html
    A  docs.20250115/_site/parsers.html
    A  docs.20250115/_site/posts/index.html
    A  docs.20250115/_site/printers.html
    A  docs.20250115/_site/redirects.json
    A  docs.20250115/_site/robots.txt
    A  docs.20250115/_site/sitemap.xml
    A  docs.20250115/_site/sw.min.js
    A  docs.20250115/_site/tags/index.html
    A  docs.20250115/_tabs/about.md
    A  docs.20250115/_tabs/archives.md
    A  docs.20250115/_tabs/categories.md
    A  docs.20250115/_tabs/tags.md
    R  docs/hooks.md -> docs.20250115/hooks.md
    A  docs.20250115/index.html
    R  docs/parsers.md -> docs.20250115/parsers.md
    R  docs/printers.md -> docs.20250115/printers.md
    A  docs.20250115/tools/run.sh
    A  docs.20250115/tools/test.sh
    A  docs/.devcontainer/devcontainer.json
    A  docs/.devcontainer/post-create.sh
    A  docs/.editorconfig
    A  docs/.gitattributes
    A  docs/.gitignore
    A  docs/.gitmodules
    M  docs/Gemfile.lock
    M  docs/_config.yml
    M  docs/_data/contact.yml
    M  docs/_data/share.yml
    A  docs/_index.html
    M  docs/index.md
    A  index.md
    M  package.json

---
+++ docs.20250115/_site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1736962431',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
