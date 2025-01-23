0a0b02f298df0b2ea98a5113c865ce7fc7dfe7ef
gesslar <1266935+gesslar@users.noreply.github.com>
2025-01-22 22:59:52 -0500

    some updates
    M  examples/node_modules_test/bedoc-wikitext-printer/bedoc-wikitext-printer.js
    M  src/core/Conveyor.js
    M  src/core/action/ParseManager.js
    M  src/core/action/PrintManager.js

--- src/core/action/PrintManager.js
+++ src/core/action/PrintManager.js
@@ -5,20 +5,21 @@ export default class PrintManager extends ActionManager {
     super(actionDefinition, logger)
   }
 
-  async print(...arg) {
+  async print(fileMap, content) {
     const log = this.log
     const debug = log.newDebug()
 
-    debug("Printing data", 2)
+    debug("Printing data for `%s`", 3, fileMap.module)
 
-    if(this.action.init)this.action.init({parent: this, log})
+    if(this.action.init)
+      this.action.init({parent: this, log})
 
     if(!this.action.print)
       throw new Error(`No print function found for action: ${this.module}`)
 
-    const result = await this.action.print(...arg)
+    const result = await this.action.print(fileMap.module, content)
 
-    debug("Print complete", 2)
+    debug("Print complete for `%s`", 3, fileMap.module)
 
     return result
   }
