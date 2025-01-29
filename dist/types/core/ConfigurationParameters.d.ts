3ac2d38c90198c9fa4f19a837178784c84f9abea
gesslar <1266935+gesslar@users.noreply.github.com>
2025-01-29 18:06:30 -0500

    Refactor TypeScript definitions and update ESLint configuration; bump version to 1.4.2

--- dist/types/core/ConfigurationParameters.d.ts
+++ dist/types/core/ConfigurationParameters.d.ts
@@ -1,36 +1,36 @@
 import TypeSpec from './util/TypeSpec';
 
 interface PathConfig {
-    type: 'file' | 'directory';
-    mustExist: boolean;
+  type: 'file' | 'directory';
+  mustExist: boolean;
 }
 
 interface ConfigurationParameter {
-    short: string;
-    param?: string;
-    description: string;
-    type: TypeSpec;
-    required: boolean;
-    default?: boolean | number | string;
-    path?: PathConfig;
-    exclusiveOf?: string;
+  short: string;
+  param?: string;
+  description: string;
+  type: TypeSpec;
+  required: boolean;
+  default?: boolean | number | string;
+  path?: PathConfig;
+  exclusiveOf?: string;
 }
 
 interface ConfigurationParametersType {
-    input: ConfigurationParameter;
-    exclude: ConfigurationParameter;
-    language: ConfigurationParameter;
-    format: ConfigurationParameter;
-    maxConcurrent: ConfigurationParameter;
-    hooks: ConfigurationParameter;
-    output: ConfigurationParameter;
-    parser: ConfigurationParameter;
-    printer: ConfigurationParameter;
-    hookTimeout: ConfigurationParameter;
-    mock: ConfigurationParameter;
-    config: ConfigurationParameter;
-    debug: ConfigurationParameter;
-    debugLevel: ConfigurationParameter;
+  input: ConfigurationParameter;
+  exclude: ConfigurationParameter;
+  language: ConfigurationParameter;
+  format: ConfigurationParameter;
+  maxConcurrent: ConfigurationParameter;
+  hooks: ConfigurationParameter;
+  output: ConfigurationParameter;
+  parser: ConfigurationParameter;
+  printer: ConfigurationParameter;
+  hookTimeout: ConfigurationParameter;
+  mock: ConfigurationParameter;
+  config: ConfigurationParameter;
+  debug: ConfigurationParameter;
+  debugLevel: ConfigurationParameter;
 }
 
 export const ConfigurationParameters: Readonly<ConfigurationParametersType>;
