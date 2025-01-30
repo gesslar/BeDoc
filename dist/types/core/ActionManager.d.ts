f362416b9ec88512d3e930ff39f4d56aca7d2d75
gesslar <1266935+gesslar@users.noreply.github.com>
2025-01-29 21:32:26 -0500

    refactor: enhance ActionManager type definitions and update version to 1.4.4 (#127)

--- dist/types/core/ActionManager.d.ts
+++ dist/types/core/ActionManager.d.ts
@@ -1,27 +1,51 @@
-import Logger from './Logger';
-import HookManager from './HookManager';
+import Logger from './Logger'
+import HookManager from './HookManager'
+
+export type MetaActionType = "print" | "parse"
+
+export interface MetaDefinition {
+  action: MetaActionType
+}
+
+export interface ParseMetaDefinition extends MetaDefinition {
+  action: "parse"
+  language: string
+}
+
+export interface PrintMetaDefinition extends MetaDefinition {
+  action: "print"
+  format: string
+}
+
+export type MetaType = ParseMetaDefinition | PrintMetaDefinition
 
 export interface ActionDefinition {
-  action: string;
-  contract: Record<string, unknown>;
-  meta: Record<string, unknown>;
+  meta: MetaType
+}
+
+export interface PrintActionDefinition {
+  meta: PrintMetaDefinition
+}
+
+export interface ParseActionDefinition {
+  meta: ParseMetaDefinition
 }
 
 export default class ActionManager {
-  constructor(actionDefinition: ActionDefinition, logger: Logger);
-  get action(): ActionDefinition;
-  set hookManager(hookManager: HookManager);
-  get hookManager(): HookManager;
-  get contract(): Record<string, unknown>;
-  get meta(): Record<string, unknown>;
-  get log(): Logger;
-  setupAction(): Promise<void>;
+  constructor(actionDefinition: ActionDefinition, logger: Logger)
+  get action(): ActionDefinition
+  set hookManager(hookManager: HookManager)
+  get hookManager(): HookManager
+  get contract(): string
+  get meta(): MetaType
+  get log(): Logger
+  setupAction(): Promise<void>
   runAction({ file, content }: {
-    file: string;
-    content: string;
-  }): Promise<string>;
-  cleanupAction(): Promise<void>;
-  toString(): string;
-  #private;
+    file: string
+    content: string
+  }): Promise<string>
+  cleanupAction(): Promise<void>
+  toString(): string
+  #private
 }
 //# sourceMappingURL=ActionManager.d.ts.map
