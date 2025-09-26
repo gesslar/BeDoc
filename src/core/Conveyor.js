c2bd90f22a6d67a0175ebc59ecf075b9f5ac48f4
codefactor-io <support@codefactor.io>
2025-09-26 00:57:11 +0000

    [CodeFactor] Apply fixes

--- src/core/Conveyor.js
+++ src/core/Conveyor.js
@@ -158,7 +158,7 @@ export default class Conveyor {
 
       return {status: "success"}
 
-    } catch(error) {
+    } catch (error) {
       return {status: "error", file, error}
     }
   }
@@ -181,7 +181,7 @@ export default class Conveyor {
       await destFile.write(destContent)
 
       return {status: "success", file: destFile}
-    } catch(error) {
+    } catch (error) {
       return {status: "error", output: destFile, error}
     }
   }
